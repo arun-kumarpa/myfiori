b3a0d96bf935eab34bd71d4335def04460c0d44a
akhilkumarjain <akhil709@yahoo.com>
2015-07-21 11:51:46 +0530

    [comment added]
    hi this is akhil is added in the file

--- project/myFiori1/myFiori1/WebContent/view/Detail.controller.js
+++ project/myFiori1/myFiori1/WebContent/view/Detail.controller.js
@@ -1,6 +1,8 @@
 jQuery.sap.require("sap.ui.demo.myFiori.util.Formatter");
 sap.ui.controller("sap.ui.demo.myFiori.view.Detail", {
 
+// hi this is akhil
+
 	handleNavButtonPress : function (evt) {
 		this.nav.back("Master");
 	}
